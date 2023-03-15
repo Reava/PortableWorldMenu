3e75474325d1ddffe8f5f8d05928360542b90c0e
Reava <reava.vr@gmail.com>
2023-03-15 22:53:06 +0100

    Extended usability of Selector script

--- Scripts/PortableMenuSelector.cs
+++ Scripts/PortableMenuSelector.cs
@@ -1,15 +1,22 @@
 ﻿using UdonSharp;
 using UnityEngine;
-using VRC.SDKBase;
-using VRC.Udon;
 
 [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
 public class PortableMenuSelector : UdonSharpBehaviour
 {
     [SerializeField] private PortableWorldMenu portableMenuSystem;
+    [Space]
+    [SerializeField] private bool isMenuSelector = true;
+    [Range(0, 4)]
     [SerializeField] private int menu = 0;
+    [Header("Send an additional event?")]
+    [SerializeField] private bool sendOptionalEvent = false;
+    [SerializeField] private string optionalEventName = "";
+    [Range(0f, 10f)]
+    [SerializeField] private float eventDelay = 0.1f;
     public override void Interact()
     {
-        portableMenuSystem._ChangeMenuTo(menu);
+        if (isMenuSelector) portableMenuSystem._ChangeMenuTo(menu);
+        if (sendOptionalEvent && optionalEventName != null) SendCustomEventDelayedSeconds(nameof(optionalEventName), eventDelay);
     }
 }
